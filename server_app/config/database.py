bc186f62ba27c30e8609cd77e220741d6284e14a
geotechblogs <ahernandez021985@gmail.com>
2023-12-27 19:33:58 +0100

    Setting the server_app to the docker-compose application

--- server_app/config/database.py
+++ server_app/config/database.py
@@ -1,8 +1,10 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm.session import sessionmaker
 
-DATABASE_URL = "postgresql+psycopg2://username:password@localhost:5432/spatial_data_db" # We will change this later after configuring the docker compose environment
+DATABASE_URL = os.getenv("DATABASE_PLANET_URL")
 engine = create_engine(DATABASE_URL)
 session = sessionmaker(bind=engine)
 Base = declarative_base()
