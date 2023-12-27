84e1d27905465e84f8df0d1dce660b940d18be1f
geotechblogs <ahernandez021985@gmail.com>
2023-12-27 23:25:28 +0100

    adding circleci configuration to test the project

--- server_app/config/database.py
+++ server_app/config/database.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm.session import sessionmaker
 
-DATABASE_URL = os.getenv("DATABASE_PLANET_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 session = sessionmaker(bind=engine)
 Base = declarative_base()
